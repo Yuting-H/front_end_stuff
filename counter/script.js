2a516e34a4b5ef6d723f495b2f84c4669b394ce8
Yuting-H <yuting.yh@gmail.com>
2024-01-04 16:14:09 -0500

    Added decrement button, counter is centered vertically

--- counter/script.js
+++ counter/script.js
@@ -1,13 +1,18 @@
 let count = 0
 
 const counter = document.getElementById("counter");
-
 const incrementButton = document.getElementById("increment");
+const decrementButton = document.getElementById("decrement");
 
-function increment() {
+function incrementCount() {
     count++;
     counter.innerHTML = count;
-    
 }
 
-incrementButton.addEventListener("click", increment);
+function decrementCount() {
+    count --;
+    counter.innerHTML = count;
+}
+
+incrementButton.addEventListener("click", incrementCount);
+decrementButton.addEventListener("click", decrementCount)
