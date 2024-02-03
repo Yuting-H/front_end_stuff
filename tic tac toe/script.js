83de44bd488b8922c24c5c9f441372ab9d74e168
yhou294 <yuting.yh@gmail.com>
2024-02-03 18:02:32 -0500

    Added Logic to reset button, addde comments

--- tic tac toe/script.js
+++ tic tac toe/script.js
@@ -3,22 +3,27 @@ let playBoxes = document.getElementsByClassName("play-box")
 
 let displayBoxes = document.getElementsByClassName("display-box")
 
+let resetButton = document.getElementById("reset")
+
 var turn = "X"
 
+// When a play box is clicked
 function onBoxClick(index){
+    
+    //return this logic 
     return function() {
 
-        
+        //if the box is empty
         if (displayBoxes[index].innerHTML == "") {
-            displayBoxes[index].innerHTML = turn
-            switchPlayer()
+            displayBoxes[index].innerHTML = turn    //draw X in box
+            switchPlayer()      //switch to O
         }
         
     }
 }
 
+//switch between the players
 function switchPlayer() {
-    console.log(turn)
     if (turn.localeCompare("X") == 0) {
         turn = "O"
     }else {
@@ -27,9 +32,18 @@ function switchPlayer() {
     }
 }
 
+function resetGame() {
+    for (let index = 0; index < displayBoxes.length; index++) {
+        displayBoxes[index].innerHTML = ""
+    }
+}
+
+//adding event listeners to each box
 for (let index = 0; index < displayBoxes.length; index++) {
     playBoxes[index].addEventListener("click", onBoxClick(index))
 }
+
+resetButton.addEventListener("click", resetGame)
 
 
 
