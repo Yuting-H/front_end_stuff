e35283c4592e6a309d57c4b207c83257593eb6a3
yhou294 <yuting.yh@gmail.com>
2024-02-02 20:50:09 -0500

    Added id to all boxes, made switching between X and O

--- tic tac toe/script.js
+++ tic tac toe/script.js
@@ -1,6 +1,36 @@
-let resetButton = document.getElementById("reset")
 
 let playBoxes = document.getElementsByClassName("play-box")
 
-let displayBoxes = document,getElementsByClassName("display-box")
+let displayBoxes = document.getElementsByClassName("display-box")
+
+var turn = "X"
+
+function onBoxClick(index){
+    return function() {
+
+        
+        if (displayBoxes[index].innerHTML == "") {
+            displayBoxes[index].innerHTML = turn
+            switchPlayer()
+        }
+        
+    }
+}
+
+function switchPlayer() {
+    console.log(turn)
+    if (turn.localeCompare("X") == 0) {
+        turn = "O"
+    }else {
+        
+        turn = "X"
+    }
+}
+
+for (let index = 0; index < displayBoxes.length; index++) {
+    playBoxes[index].addEventListener("click", onBoxClick(index))
+}
+
+
+
 
