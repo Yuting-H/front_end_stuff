29ef2338cbffc02fb5c8b9b96924af9f3ec01b23
Yuting Hou <yhou294@uwo.ca>
2024-02-17 12:34:39 -0500

    Created bounding box

--- Vanilla JS/moving game/script.js
+++ Vanilla JS/moving game/script.js
@@ -1,10 +1,18 @@
 let player = document.getElementById("player");
 
+let playerWidth = player.getBoundingClientRect().width;
+
+let playerHeight = player.getBoundingClientRect().height
+
+var maxWidth = window.innerWidth;
+
+var maxheight = window.innerHeight;
+
 var xPos = 0;
 
 var yPos = 0;
 
-var speed = 2.5;
+var speed = 5;
 
 //records which key is pressed
 var keyDowns = {
@@ -69,10 +77,10 @@ window.addEventListener("keyup", (event) => {
 
 function gameLoop() {
 
+    //determines which keys are pressed
     if(keyDowns.w == true) {
         yPos = yPos - 1*speed;
     }
-    
     if(keyDowns.a == true) {
         xPos = xPos - 1*speed;
     }
@@ -82,10 +90,21 @@ function gameLoop() {
     if(keyDowns.d == true) {
         xPos = xPos + 1*speed;
     }
-    
 
-    console.log(keyDowns.w)
+    if (xPos > maxWidth - playerWidth) {
+        xPos = xPos -1 * speed;
+    }
+    if (xPos < 0) {
+        xPos = xPos + 1 *speed;
+    }
+    if (yPos > maxheight - playerHeight) {
+        yPos = yPos - 1 * speed;
+    }
+    if (yPos < 0) {
+        yPos = yPos + 1 * speed;
+    }
 
+    //moves the player
     player.style.transform = `translate(${xPos}px, ${yPos}px)`
 }
 
