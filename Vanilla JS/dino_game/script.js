489c9e7563f1f3a2cb9af6675a7665737ff6763c
yhou294 <yuting.yh@gmail.com>
2024-04-08 23:16:48 -0400

    Update script.js

--- Vanilla JS/dino_game/script.js
+++ Vanilla JS/dino_game/script.js
@@ -6,17 +6,34 @@ let dinoBox = dino.getBoundingClientRect();
 
 let groundY = ground.getBoundingClientRect().top;
 
-var dino_Y = groundY + dinoBox.height;
+let jumpHeight= 100;
 
-dino.style.transform = `translate(100px, ${groundY - dinoBox.height}px)`
+var dino_Y = dinoBox.bottom + dinoBox.height;
+
+dino.style.transform = `translate(100px, ${dino_Y}px)`
+
+var keyDowns = {};
 
 window.addEventListener("keydown", (event) => {
 
-    console.table(event);
+    console.table(event.key);
     
     //if space is pressed
-    if(event.key = " ") {
-        dino_Y + 10
+    if(event.key == ' ') {
+        console.log(dino_Y)
+        dino_Y = dino_Y - jumpHeight;
     }
-
 })
+
+//game loop
+function updateGame() {
+    console.log(dino_Y)
+    dino.style.transform = `translate(100px, ${dino_Y - dinoBox.height}px)`;
+
+    if(dino_Y < groundY){
+        
+        dino_Y++;
+    }
+}
+
+setInterval(updateGame, 10);
