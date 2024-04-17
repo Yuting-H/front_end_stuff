c87ae518f7cd1d9e3ac9a356373c4ae91cb876df
yhou294 <yuting.yh@gmail.com>
2024-04-17 15:37:57 -0400

    update dino

--- Vanilla JS/dino_game/script.js
+++ Vanilla JS/dino_game/script.js
@@ -6,34 +6,37 @@ let dinoBox = dino.getBoundingClientRect();
 
 let groundY = ground.getBoundingClientRect().top;
 
-let jumpHeight= 100;
+let jumpHeight = 100;
 
-var dino_Y = dinoBox.bottom + dinoBox.height;
+var dino_Y = dinoBox.bottom;
 
-dino.style.transform = `translate(100px, ${dino_Y}px)`
+dino.style.transform = `translate(100px, ${dino_Y}px)`;
 
-var keyDowns = {};
+var keyDowns = {
+  jump: false,
+};
 
 window.addEventListener("keydown", (event) => {
+  console.table(event.key);
 
-    console.table(event.key);
-    
-    //if space is pressed
-    if(event.key == ' ') {
-        console.log(dino_Y)
-        dino_Y = dino_Y - jumpHeight;
-    }
-})
+  //if space is pressed
+  if (event.key == " ") {
+    console.log(dino_Y);
+    dino_Y = dino_Y - jumpHeight;
+  }
+});
+
+window.addEventListener("keyup", (event) => {});
 
 //game loop
 function updateGame() {
-    console.log(dino_Y)
-    dino.style.transform = `translate(100px, ${dino_Y - dinoBox.height}px)`;
+  console.log(dino_Y);
+  dino.style.transform = `translate(100px, ${dino_Y - dinoBox.height}px)`;
 
-    if(dino_Y < groundY){
-        
-        dino_Y++;
-    }
+  //Updates position of dino
+  if (dino_Y < groundY) {
+    dino_Y++;
+  }
 }
 
 setInterval(updateGame, 10);
